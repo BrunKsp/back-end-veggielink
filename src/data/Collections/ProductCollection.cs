d8a191eb0aac271ceb05fdcef8e2ebb75c715ce7
Bruno <bbrunosiqueira14@gmail.com>
2024-06-08 22:31:29 -0300

    feat: products

--- src/data/Collections/ProductCollection.cs
+++ src/data/Collections/ProductCollection.cs
@@ -13,8 +13,10 @@ public class ProductCollection
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? Thumb { get; set; }
+    public string Fertilizer { get; set; }
+    public string Observation { get; set; }
     public DateTime PlantingDate { get; set; }
     public DateTime HarverstDate { get; set; }
-    [BsonRepresentation(BsonType.ObjectId)]
+    public DateTime PreparingDate { get; set; }
     public string CategoryId { get; set; }
 }
